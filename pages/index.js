8bc65e2448cd564306b6bfe3ce22eeae41aad414
SouresSam <samara.soares1032@gmail.com>
2025-10-08 20:18:22 -0300

    .

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
-function Home () {
-  return <>
-  <h1>filmes</h1>;
-  
-  <p>Listagem de filmes aqui</p>
+import { filmes } from '../data/filmes';
 
-  </>
+function Home () {
+    console.log(filmes);
+
+    const primeiroFilme = filmes[0];
+
+    const estiloImagem = {
+        width: "300px"
+    };
+
+    return (
+        <>
+            <h1>Filmes</h1>
+
+            <ul>
+                            <li>
+                                <p>Título: {primeiroFilme.titulo}</p>
+                                <p>Ano: {primeiroFilme.ano}</p>
+                                <p>Gênero: {primeiroFilme.genero}</p>
+                                <p>Nota: {primeiroFilme.nota}</p>
+
+                                <img style={estiloImagem} src={primeiroFilme.poster}/>
+                            </li>
+            </ul>
+        </>
+    );
 }
 
-export default Home; 
+export default Home;
