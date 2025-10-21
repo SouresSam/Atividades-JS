0050ca61c26790790c528b95fd8f85c98cb745b7
SouresSam <samara.soares1032@gmail.com>
2025-10-20 21:59:56 -0300

    .

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
 import { filmes } from '../data/filmes';
 
+function CardFilme({filme}) {
+        const estiloImagem = {
+        width: "300px"
+     };
+
+    return (     
+    <li> 
+        <p>Título: {filme.titulo}</p>
+        <p>Ano: {filme.ano}</p>
+        <p>Gênero: {filme.genero}</p>
+        <p>Nota: {filme.nota}</p>
+
+        <img style={estiloImagem} src={filme.poster}/>
+    </li>
+    );
+}
+
 function Home () {
 
-    const estiloImagem = {
-        width: "300px"
-    };
     return (
         <>
             <h1>Filmes</h1>
@@ -12,14 +26,9 @@ function Home () {
             <ul>
                  { 
                   filmes.map(( filme ) => {
-                        return (  <li key= { `card-filme-${ filme.id }`}> 
-                            <p>Título: {filme.titulo}</p>
-                            <p>Ano: {filme.ano}</p>
-                            <p>Gênero: {filme.genero}</p>
-                            <p>Nota: {filme.nota}</p>
-
-                            <img style={estiloImagem} src={filme.poster}/>
-                        </li>);
+                        return (
+                          <CardFilme key= { `card-filme-${ filme.id }`} filme={filme} />  
+                        );
                   })
                   } 
             </ul>
